43a15b5f75e5f8a8b42c77a88be2f1a81c77dffa
tilensotlar <tilen.sotlar@student.um.si>
2025-05-07 21:14:50 +0200

    delujoče VSE!!!!
    spletne strani main, izdelki pa se o nas so delujoči, manjka le še tista češnja za na vrh kero pa bom delu do 2eh zjutri i can feel it :)

---
+++ src/costum.js
@@ -0,0 +1,24 @@
+const searchInput = document.querySelector("[data-search]");
+const cards = document.querySelectorAll(".card");
+const filterDropdown = document.getElementById("filterDropdown");
+
+function filterCards(value) {
+  const searchValue = value.toLowerCase();
+  cards.forEach(card => {
+    const header = card.querySelector(".header");
+    const isVisible = header && header.textContent.toLowerCase().includes(searchValue);
+    card.classList.toggle("hide", !isVisible);
+  });
+}
+
+// Input search 
+searchInput.addEventListener("input", e => {
+  filterDropdown.value = "";
+  filterCards(e.target.value);
+});
+
+// Dropdown filtering
+filterDropdown.addEventListener("change", e => {
+  searchInput.value = ""; 
+  filterCards(e.target.value);
+});
