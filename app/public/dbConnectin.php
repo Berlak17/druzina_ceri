8e5f42ccb0fe7c7f7eacd554a9ba406ff2028b03
Berlak17 <tristanberlak9@gmail.com>
2025-06-04 13:42:31 +0200

    Pt2

--- app/public/dbConnectin.php
+++ app/public/dbConnectin.php
@@ -1,11 +1,11 @@
 <?php 
 
-$servername = "localhost";
-$username = "tristan";
-$password = "";
+$servername = "127.0.0.1";
+$username = "root";
+$password = "root";
 
 try {
-    $conn = new PDO("mysql:host=$servername:3306;dbname=druzinaceri", $username, $password);
+    $conn = new PDO("mysql:localhost;dbname=druzinaceri", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
 } catch(PDOException $e) {
